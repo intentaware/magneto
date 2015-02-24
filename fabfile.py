3fce1e5a3e2951e87ad0fd3847714801b7e85be8
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-02-25 02:10:55 +0500

    conf | updated fabric

--- fabfile.py
+++ fabfile.py
@@ -30,6 +30,27 @@ def local():
     env.venv_root = LOCAL_ENVIRONMENT_PATH
     env.venv = 'source %(venv_root)sbin/activate && ' % env
 
+def stage():
+    '''
+    Environment Settings for Staging Server
+
+    Usage:
+        fab stage <task>
+    '''
+    env.run = run
+    env.name = 'adomattic-stage'
+    env.conf_path = 'stage'
+    env.project_root = '/srv/%(name)s/' % env
+    env.hosts = ['app.adomattic.com']
+    env.user = 'root'
+    env.key_filename = DEPLOY_KEY
+    env.branch = 'dev'
+    env.venv_root = '/srv/%(name)s/' % env
+    env.venv = 'source /srv/%(name)s/bin/activate && ' % env
+    #env.backoffice = '/srv/%(name)s/apps/backoffice/static/backoffice/' % env
+
+
+
 def update_envs():
     '''
     Updates local environment settings to the default repo settings, useful for parallel programming
@@ -37,3 +58,21 @@ def update_envs():
         fab local update_envs
     '''
     env.run('cp adomatic/conf/%(conf_path)s/settings.py adomatic/settings/local.py' % env)
+
+
+def prepare():
+    """
+    For things that need to be installed via apt-get.
+    These are installed before requirements.txt in the venv otherwise some python modules won't install properly
+    """
+    env.run('sudo apt-get update')
+    env.run("sudo apt-get install locate python-setuptools git-core subversion mercurial htop screen byobu gcc")
+    env.run('sudo apt-get install libjpeg62 libjpeg62-dev zlib1g-dev libfreetype6 libfreetype6-dev python-pycurl-dbg libcurl4-openssl-dev')
+    env.run('sudo apt-get install libpq-dev python-dev')
+    env.run('sudo apt-get install libxml2-dev libxslt-dev')
+    env.run('sudo apt-get install nginx-full uwsgi uwsgi-plugin-python')
+    env.run('sudo apt-get install python-pip')
+    env.run('sudo apt-get install libreadline6 libreadline6-dev libncurses5-dev')
+    env.run('sudo pip install virtualenv')
+    env.run('sudo apt-get install nodejs-legacy')
+    env.run('sudo npm i -g bower gulp yo')
