34e3535b75185519135488d2ecf2227bcd21f552
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-03-06 05:35:20 +0500

    ads | full form

---
+++ apps/dashboard/static/dash/src/js/dashboard/controllers/forms/create-ad.js
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * Created by yousuf on 06/03/2015.
+ */
+
+angular.module('adomattic.dashboard')
+  .controller('CreateAdFormCtrl', function($scope) {
+    console.log('i am the form');
+    console.log($scope);
+  });
