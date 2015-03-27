8318d8a71f8d585cde60983383384cfac60004fc
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-03-27 17:23:14 +0500

    comapnies | setting the pub key

--- apps/companies/migrations/0005_company_publisher_key.py
+++ apps/companies/migrations/0005_company_publisher_key.py
@@ -4,6 +4,15 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django_extensions.db.fields
 
+import shortuuid
+
+def set_key(apps, schema_editor):
+    Company = apps.get_model('companies', 'Company')
+
+    for c in Company.objects.all():
+        c.publisher_key = shortuuid.uuid()
+        c.save()
+
 
 class Migration(migrations.Migration):
 
@@ -18,4 +27,5 @@ class Migration(migrations.Migration):
             field=django_extensions.db.fields.ShortUUIDField(null=True, editable=False, blank=True),
             preserve_default=True,
         ),
+        migrations.RunPython(set_key),
     ]
