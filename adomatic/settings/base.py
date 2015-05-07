0a8f88f973b017e38a6a9f9766daa2446a5b5f47
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-05-07 22:47:02 +0500

    mandrill settings

--- adomatic/settings/base.py
+++ adomatic/settings/base.py
@@ -160,7 +160,7 @@ CORS_ALLOW_HEADERS = (
 IMPRESSION_COOKIE_NAME = 'magneto'
 
 # Madrill
-MANDRILL_API_KEY = "_3SpFp-4JKwI-c5k7CqWwQ"
+MANDRILL_API_KEY = "US9U8XiepMg6nUVDaq5UeQ"
 EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 ADOMATTIC_FROM = "Adomattic <noreply@adomattic.com>"
 
