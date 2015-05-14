2dc7f3de5547e5c889bcc0b36639a0114a29b432
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-05-14 10:57:54 +0500

    financials | Stripe class, errors removed

--- apps/finances/mixins.py
+++ apps/finances/mixins.py
@@ -6,35 +6,38 @@ class Stripe(object):
     class to instantiate stripe and provide methods for error handling
     """
 
-    def __init__(*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         stripe.api_key = settings.STRIPE_KEY
         self._stripe = stripe
         # params is a dictionary used to set parameters during a charge life
         # cycle, mostly used in post_save and pre_save methods
-        self._params = Dict()
+        self._params = dict()
 
-    def pre_charge(*args, **kwargs):
+    def pre_charge(self, *args, **kwargs):
         """
         to perform before preparing a charge,
         used to set self._params parameter
         """
         pass
 
-    def charge(self, amount_in_cents, currency, description, *args, **kwargs):
+    def charge(self, amount_in_cents, description, currency=None, *args, **kwargs):
         self.pre_charge(*args, **kwargs)
+        if not currency:
+            currency = 'CAD'
         try:
-            response = self._stripe.Charge.create(
+            self._params = self._stripe.Charge.create(
                 amount=amount_in_cents,
-                currency='CAD',
+                currency=currency,
                 description=description,
-                self._params,
+                **self._params
             )
             self.post_charge(*args, **kwargs)
-            return self._params = True, response
+            return True, self._params
         except self._stripe.error.CardError, ce:
-            return self._params = False, ce
+            self._params = ce
+            return False, self._params
 
-    def post_charge(**args, **kwargs):
+    def post_charge(self, *args, **kwargs):
         """
         to perform after the charge
         """
