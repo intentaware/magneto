4626a3fea279c43333dd5c77200c544ff0103fa8
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-06-03 00:34:48 +0500

    urls | removed typo

--- apps/impressions/api/urls.py
+++ apps/impressions/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
     'impressions',
     #url(r'^register/user/$', UserRegistrationView.as_view(), name='register_user'),
     url(r'^i/$', GetImpression.as_view(), name='get_new_impression'),
-    url(r'^i/(?P<pk>\d+)/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', GetImpression.as_view(), name='claim_coupon')
+    url(r'^i/(?P<pk>\d+)/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', GetImpression.as_view(), name='claim_coupon'),
 )
