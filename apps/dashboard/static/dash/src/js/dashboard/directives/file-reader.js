4c75a5eaba4c50ae1dfaf0ae0c879cbed7bc415e
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-06-03 18:09:13 +0500

    dashboard | the image reader directive in the style of angular material

--- apps/dashboard/static/dash/src/js/dashboard/directives/file-reader.js
+++ apps/dashboard/static/dash/src/js/dashboard/directives/file-reader.js
@@ -7,14 +7,18 @@ angular.module('adomattic.dashboard')
       scope: {
         label: '@',
         accept: '@',
+        ngModel: '=',
       },
       templateUrl: urls.partials.directives + 'file-reader.html',
       replace: true,
-      require: '?ngModel',
-      link: function(scope, elm, atr, ngModel) {
+      require: '^ngModel',
+      link: function(scope, elm, atr, ngModelCtrl) {
+        ngModelCtrl.$render = function() {};
+        //scope.fileName = '';
         var $file = elm[0].querySelector('.overlay');
         console.log($file);
 
+        // watch for a new file in input
         $file.onchange = function (e) {
           var el = e.target;
           console.log(el.value);
@@ -24,9 +28,39 @@ angular.module('adomattic.dashboard')
           }
 
           console.log(scope);
-          scope.fileName = el.value;
+          console.log(el.files);
           //scope.apply();
-        }
+          //
+          var name = el.files[0].name;
+
+          $q.all(Array.prototype.slice.call(el.files, 0).map(read))
+            .then(function(vals) {
+              scope.fileName = name;
+              ngModelCtrl.$setViewValue(vals.length ? vals[0] : null)
+            }, function() {
+              scope.fileName = 'Unable to load file, please try again';
+              scope.$apply();
+            });
+        };
+
+        // return the file contents in base64 format
+        var read = function (f) {
+          console.log(f);
+          var d = $q.defer();
+          var r = new FileReader();
+
+          r.onload = function(ev) {
+            d.resolve(ev.target.result);
+          }
+
+          r.onerror = function(ev) {
+            d.reject(ev);
+          }
+
+          r.readAsDataURL(f);
+
+          return d.promise;
+        };
         /*
         var bindElement = function() {
           ngModel.$render = function() {};
