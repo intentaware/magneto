b122e710eda44f82d3b2e9d4f728ed4fff8d44f9
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-06-12 03:11:44 +0500

    more comprehensive log file

--- adomattic/conf/stage/settings.py
+++ adomattic/conf/stage/settings.py
@@ -35,30 +35,57 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
         },
     },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
-        'file': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        # I always add this handler to facilitate separating loggings
+        'log_file':{
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/django-errors.log'),
+            'maxBytes': '16777216', # 16megabytes
             'formatter': 'verbose'
         },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        }
     },
     'loggers': {
-        'django': {
-            'handlers':['file'],
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
             'propagate': True,
-            'level':'DEBUG',
         },
-        'apps': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
+        'apps': { # I keep all my of apps under 'apps' folder, but you can also add them one by one, and this depends on how your virtualenv/paths are set
+            'handlers': ['log_file'],
+            'level': 'INFO',
+            'propagate': True,
         },
-    }
+    },
+    # you can also shortcut 'loggers' and just configure logging for EVERYTHING at once
+    'root': {
+        'handlers': ['console', 'mail_admins'],
+        'level': 'INFO'
+    },
 }
