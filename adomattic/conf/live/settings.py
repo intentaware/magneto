438fe2a64c4ff0e22ba5486c99cded395e1f920a
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-06-21 07:18:00 +0500

    storages | storage configuration for different root for media and static in same bucket

--- adomattic/conf/live/settings.py
+++ adomattic/conf/live/settings.py
@@ -20,7 +20,7 @@ RAVEN_CONFIG = {
     'dsn': 'https://b1b9cb8df048483295724db97fadd76c:7f95b0b35c154327a0cd24d6638eb1d8@app.getsentry.com/45852',
 }
 
-AWS_STORAGE_BUCKET_NAME = 'vader-faith'
+AWS_STORAGE_BUCKET_NAME = 'admtc'
 AWS_ACCESS_KEY_ID = 'AKIAI23OY6DM5YW6IWIA'
 AWS_SECRET_ACCESS_KEY = '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'
 
@@ -30,16 +30,22 @@ AWS_SECRET_ACCESS_KEY = 'HAxJNdN51hYh6gpg1GjVP8AqIONBXZTJrjby/XFX'
 # We also use it in the next setting.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+# Location for static files root in your s3 bucket
+STATICFILES_LOCATION = 'static'
+
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'apps.common.utils.StaticStorage'
 
-STATIC_URL = 'http://app.adomattic.com/static/'
-MEDIA_URL = 'http://app.adomattic.com/media/'
+MEDIAFILES_LOCATION = 'media'
+
+DEFAULT_FILE_STORAGE = 'apps.common.utils.MediaStorage'
+
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 DATABASES = {
     'default': {
