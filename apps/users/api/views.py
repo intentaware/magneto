38183ed3e2c6fcf7cf53f03e14ee1dcc9316d8ab
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-06-28 18:23:58 +0500

    dynamic path

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from registration import signals
 
 from apps.api.permissions import UserRegistrationAPIPermission, \
     PublisherAPIPermission
@@ -27,6 +28,11 @@ class CompanyRegistrationView(APIView):
         serializer = CompanyRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             member = serializer.save()
+            signals.user_registered.send(
+                sender=self.__class__,
+                user=member.user,
+                request=request
+            )
             return Response({
                 'company': member.company.id
             }, status=201)
