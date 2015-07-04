fc2968b8dc1bb59ab282bd1db438ebe7dc703ec4
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-07-04 18:17:52 +0500

    dashboard | editing campaign

--- apps/dashboard/static/dash/src/js/dashboard/controllers/routes/campaigns/create.js
+++ apps/dashboard/static/dash/src/js/dashboard/controllers/routes/campaigns/create.js
@@ -3,7 +3,6 @@
 angular.module('adomattic.dashboard')
   .controller('CampaignCreateCtrl', function($rootScope) {
     var self = this;
-    console.log('testing the controller as');
     $rootScope.$on('campaginFormUpdated', function(e, args){
       self.ad = args;
       //console.log(args);
