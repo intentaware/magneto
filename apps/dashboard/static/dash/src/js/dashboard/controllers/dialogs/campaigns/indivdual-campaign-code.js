aaf2abc975ccd2b3f9a08be38d30e7c8b30a68d7
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-07-11 02:58:46 +0500

    working coupon code

--- apps/dashboard/static/dash/src/js/dashboard/controllers/dialogs/campaigns/indivdual-campaign-code.js
+++ apps/dashboard/static/dash/src/js/dashboard/controllers/dialogs/campaigns/indivdual-campaign-code.js
@@ -4,11 +4,14 @@
  */
 
 angular.module('adomattic.dashboard')
-  .controller('CampaignIndividualIncludeCodeDialogCtrl', function ($mdDialog, campaignID) {
+  .controller('CampaignIndividualIncludeCodeDialogCtrl', function ($mdDialog, $rootScope, campaignID) {
     var self = this;
     self.campaignID = campaignID;
 
     self.hide = function () {
       $mdDialog.hide();
     };
+
+    self.assetRoot = (window.location.hostname === 'app.adomattic.com') ? 'http://app.adomattic.com/magneto/' : 'http://' + window.location.host + '/static/impressions/dist/';
+    self.globals = $rootScope.globals;
   });
