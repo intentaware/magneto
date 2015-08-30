7c28f4b09d333fda1098f22c5db7c2b60b15f05b
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-08-30 12:49:44 +0500

    stage | conf | moved staging to intentaware

--- adomattic/conf/stage/settings.py
+++ adomattic/conf/stage/settings.py
@@ -5,11 +5,11 @@ SITE_ID = 1
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '.adomattic.com',  # Allow domain and subdomains
-    '.adomattic.com.',  # Also allow FQDN and subdomains
+    '.intentaware.com',  # Allow domain and subdomains
+    '.intentaware.com.',  # Also allow FQDN and subdomains
 ]
 
-BASE_URL = "http://stage.adomattic.com"
+BASE_URL = "http://stage.intentaware.com"
 
 INSTALLED_APPS += (
     'rest_framework',
@@ -22,8 +22,8 @@ RAVEN_CONFIG = {
     'dsn': 'https://b1b9cb8df048483295724db97fadd76c:7f95b0b35c154327a0cd24d6638eb1d8@app.getsentry.com/45852',
 }
 
-STATIC_URL = 'http://stage.adomattic.com/static/'
-MEDIA_URL = 'http://stage.adomattic.com/media/'
+STATIC_URL = 'http://stage.intentaware.com/static/'
+MEDIA_URL = 'http://stage.intentaware.com/media/'
 
 DATABASES = {
     'default': {
