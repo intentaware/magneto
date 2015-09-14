7a1e1a1db1140041944d036c1cdb145fc856320e
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-09-15 01:36:16 +0500

    circles | companies | fixing api

--- apps/companies/api/routes.py
+++ apps/companies/api/routes.py
@@ -1,6 +1,5 @@
 from rest_framework.routers import SimpleRouter
-from .viewsets import CompanyViewSet, CircleViewSet
+from .viewsets import CompanyViewSet
 
 router = SimpleRouter()
 router.register('companies', CompanyViewSet, base_name='campanies')
-router.register('circles', CircleViewSet)
