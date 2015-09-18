955e8eaa9ba0b31e706d42c9315ccc47fd8d4568
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-09-18 19:49:06 +0500

    metas | Circle Admin

--- apps/metas/admin.py
+++ apps/metas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class CircleAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Circle._meta.fields]
+
+
+admin.site.register(Circle, CircleAdmin)
