6e7b1f8d28c94933c8f708af3e4353453fd6e4a7
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-10-19 02:30:28 +0500

    import/export | quick admin integeration

--- apps/campaigns/admin.py
+++ apps/campaigns/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from import_export import resources, admin as ie_admin
 
 from .models import *
 
+class CampaignResource(resources.ModelResource):
+    class Meta:
+        model = Campaign
 
-class CampaignAdmin(admin.ModelAdmin):
+class CouponResource(resources.ModelResource):
+    class Meta:
+        model = Coupon
+
+
+class CampaignAdmin(ie_admin.ExportActionModelAdmin):
+    resource_class = CampaignResource
     list_display = [f.name for f in Campaign._meta.fields]
 
 
-class CouponAdmin(admin.ModelAdmin):
+class CouponAdmin(ie_admin.ExportActionModelAdmin):
     list_display = [f.name for f in Coupon._meta.fields]
+    resource_class = CouponResource
 
 
 admin.site.register(Campaign, CampaignAdmin)
--- apps/impressions/admin.py
+++ apps/impressions/admin.py
@@ -1,10 +1,14 @@
-from django.contrib import admin
+from import_export import resources, admin as ie_admin
 
 from .models import Impression
 
+class ImpressionResource(resources.ModelResource):
+    model = Impression
 
-class ImpressionAdmin(admin.ModelAdmin):
+
+class ImpressionAdmin(ie_admin.ExportActionModelAdmin):
     list_display = [f.name for f in Impression._meta.fields]
+    resource_class = ImpressionResource
 
 
 admin.site.register(Impression, ImpressionAdmin)
