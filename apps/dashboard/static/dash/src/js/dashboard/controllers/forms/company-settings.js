f429f7509039de2a55b1c94fca49f68dd98f90a8
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-10-24 21:20:15 +0500

    circles | helper function refactor

--- apps/dashboard/static/dash/src/js/dashboard/controllers/forms/company-settings.js
+++ apps/dashboard/static/dash/src/js/dashboard/controllers/forms/company-settings.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('adomattic.dashboard')
-  .controller('SettingsFormCtrl', function($rootScope, $location, Company) {
+  .controller('SettingsFormCtrl', function($rootScope, $location, Company, Circle) {
+
     var self = this;
     self.company = $rootScope.globals.company;
 
+    self.circles = [];
+
+    Circle.query().$prome.then(function(response) {
+      self.circles = response;
+    });
+
     self.updateSettings = function () {
-      console.log('i am here');
-      Company.update(self.company).$promise.then(function () {
+      console.log(self.company);
+      Company.update(self.company).$promise.then(function (response) {
+        console.log(response);
         $location.path('/');
       });
     };
