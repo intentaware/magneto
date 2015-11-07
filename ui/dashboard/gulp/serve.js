5f17d1ab58847b231ec3d140537e31388a637627
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-08 03:53:41 +0500

    the complete build process

--- ui/dashboard/gulp/serve.js
+++ ui/dashboard/gulp/serve.js
@@ -24,7 +24,23 @@ function browserSyncInit(baseDir, files, browser) {
   });
 }
 
+// task to serve development with live refresh
 gulp.task('serve', ['watch', 'copy'], function () {
+  browserSyncInit([], [
+    paths.django.assets.dashboard + '/**/*.css',
+    paths.django.assets.dashboard + '/**/*.js',
+    '!' + paths.django.assets.dashboard + '/**/vendor.js',
+    paths.django.templates.root + '/**/*.html'
+    //paths.src + '/{app,components}/**/*.js',
+    //paths.src + 'src/assets/images/**/*',
+    //paths.compile + '/serve/*.html',
+    //paths.compile + '/serve/{app,components}/**/*.html',
+    //paths.src + '/{app,components}/**/*.html'
+  ]);
+});
+
+// task to quick preview the build files
+gulp.task('preview', ['build'], function () {
   browserSyncInit([], [
     paths.django.assets.dashboard + '/**/*.css',
     paths.django.assets.dashboard + '/**/*.js',
