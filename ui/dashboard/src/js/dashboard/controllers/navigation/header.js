1d16c5270ba15644fc1858adb80da0dfd6e871e2
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-09 06:27:38 +0500

    decouple / moving out ui
    ui will now have a new repository.

--- ui/dashboard/src/js/dashboard/controllers/navigation/header.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('adomattic.dashboard')
-  .controller('HeaderNavigationCtrl', function($scope, $mdSidenav) {
-    $scope.openLeftMenu = function() {
-      $mdSidenav('left').toggle();
-    };
-  });
