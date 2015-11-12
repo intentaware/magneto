6fa6d8ecc520aad6f7be1dc2561c1bc3f25c0503
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-13 03:44:06 +0500

    by campaign admin

--- apps/impressions/admin.py
+++ apps/impressions/admin.py
@@ -10,7 +10,7 @@ class ImpressionResource(resources.ModelResource):
 
 class ImpressionAdmin(ie_admin.ExportActionModelAdmin):
     list_display = ['id', 'added_on', 'campaign', 'publisher', 'meta']
-    list_filter = ['campaign__name', 'publisher']
+    list_filter = ['campaign', 'publisher']
     resource_class = ImpressionResource
     pass
 
