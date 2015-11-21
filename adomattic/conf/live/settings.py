9b38748edbee4f1b156174a1786348a6f81b50fb
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-21 11:12:24 +0500

    simple formatted on live

--- adomattic/conf/live/settings.py
+++ adomattic/conf/live/settings.py
@@ -63,7 +63,10 @@ LOGGING = {
         'verbose': {
             'format': '[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
             'datefmt': '%Y-%m-%d %H:%M:%S'
-        }
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
     },
     'handlers': {
         # Include the default Django email handler for errors
