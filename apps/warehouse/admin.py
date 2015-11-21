51c6282e0caab39bd1cf15d9bc8c909d7861071f
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-21 10:40:54 +0500

    IPSTORE admin

--- apps/warehouse/admin.py
+++ apps/warehouse/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import IPStore
 
 # Register your models here.
+
+class IPStoreAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in IPStore._meta.fields]
+
+admin.site.register(IPStore, IPStoreAdmin)
