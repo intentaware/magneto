7b5bf7c582f2f3b29cdc35a573ba07775407a12d
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-21 14:19:44 +0500

    ipstore | admin | filter on country

--- apps/warehouse/admin.py
+++ apps/warehouse/admin.py
@@ -6,5 +6,6 @@ from .models import IPStore
 class IPStoreAdmin(admin.ModelAdmin):
     list_display = ['ip', 'postal_code', 'country', 'added_on', 'updated_on']
     ordering = ['postal_code__country']
+    list_filter = ['postal_code__country']
 
 admin.site.register(IPStore, IPStoreAdmin)
