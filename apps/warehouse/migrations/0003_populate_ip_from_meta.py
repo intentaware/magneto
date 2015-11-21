56334e2af9733fd8f2ac67d57e48d989c8680050
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-21 10:18:09 +0500

    migrations fix

--- apps/warehouse/migrations/0003_populate_ip_from_meta.py
+++ apps/warehouse/migrations/0003_populate_ip_from_meta.py
@@ -6,32 +6,34 @@ from django.db import models, migrations
 def populate_data(apps, schemaeditor):
     Impression = apps.get_model('impressions', 'Impression')
     IPStore = apps.get_model('warehouse', 'IPStore')
-
+    print ''
     from plugins.cities.models import Country, PostalCode
 
     for i in Impression.objects.filter(meta__at_ip__isnull=False, meta__at_ip2geo__isnull=False):
         country_name = name=i.meta['ip2geo']['country']['names']['en']
-        print ''
         try:
             country = Country.objects.get(name=country_name)
         except:
             print country_name
             country = None
 
-        postal_code = PostalCode.objects.filter(
+        try:
+            postal_code = PostalCode.objects.filter(
                 code = i.meta['ip2geo']['postal']['code'],
                 country = country
             )
-        if postal_code.count() > 0:
-            postal_code_id = postal_code[0].id
-            print postal_code_id
-        else:
-            postal_code_id = None
+            if postal_code.count() > 0:
+                postal_code_id = postal_code[0].id
+                print postal_code_id
+            else:
+                postal_code_id = None
 
-        ip, created = IPStore.objects.get_or_create(ip=i.meta['ip'])
-        if postal_code_id:
-            ip.postal_code_id=postal_code_id
-            ip.save()
+            ip, created = IPStore.objects.get_or_create(ip=i.meta['ip'])
+            if postal_code_id:
+                ip.postal_code_id=postal_code_id
+                ip.save()
+        except:
+            print i.meta
 
 
 class Migration(migrations.Migration):
