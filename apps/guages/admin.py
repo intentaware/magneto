1a55fffdea34205d2a609d78989fdea600913085
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-11-30 22:04:13 +0500

    Matric Admin enabled

--- apps/guages/admin.py
+++ apps/guages/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Asset
+from .models import Asset, Matric
 # Register your models here.
 
 class AssetAdmin(admin.ModelAdmin):
     list_display = ['id', 'key', 'publisher', 'url']
     list_display_links = ['id', 'url']
 
+class MatricAdmin(admin.ModelAdmin):
+    list_display = ['id', 'asset', 'visitor', 'meta']
+
 admin.site.register(Asset, AssetAdmin)
+admin.site.register(Matric, MatricAdmin)
