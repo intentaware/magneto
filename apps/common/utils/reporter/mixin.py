0c4488d6546d9965b70254e553127fe7e870806e
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-12-02 17:48:37 +0500

    stupid mistake

--- apps/common/utils/reporter/mixin.py
+++ apps/common/utils/reporter/mixin.py
@@ -69,9 +69,9 @@ class Reporter(object):
         import datetime
         _now = datetime.datetime.now()
 
-        for ip in IPStore.objects.filter(added_on__month=_now.month):
+        for ip in IPStore.objects.filter(geocode__isnull=True):
             qs = queryset.filter(
-                meta__at_ip=ip).order_by('-added_on')
+                meta__at_ip=ip.ip).order_by('-added_on')
             if qs.count() > 1:
                 obj = qs[0]
             else:
