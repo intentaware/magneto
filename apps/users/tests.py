382142d8846dcb4b64ac6569181e25759af962c4
Rehman <manichilll@yahoo.com>
2016-01-07 20:56:47 +0500

    Users tests

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from models import *
+import factory
 
+class UserFactory(factory.Factory):
+	FACTORY_FOR = Users
+
+	first_name = factory.Sequence(lambda n: "Agent %03d" % n)
+	last_name = 'Steve'
+	email = factory.LazyAttribute(lambda obj: '%s@example.com' % obj.first_name)
+
+	class UserTest(TestCase):
+		user	=	factories.UserFactory.create()
+        self.client.login(username=user.username, password='abc')
 # Create your tests here.
