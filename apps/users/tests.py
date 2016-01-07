c14eb1e7998565a2fb68b66a9b6c27baf5ae4fcf
Rehman <manichilll@yahoo.com>
2016-01-08 02:08:04 +0500

    User Factory made , user login test successful

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,15 +1,21 @@
 from django.test import TestCase
-from models import *
+from models import User
 import factory
 
 class UserFactory(factory.Factory):
-	FACTORY_FOR = Users
+    class Meta:
+        model = User
 
-	first_name = factory.Sequence(lambda n: "Agent %03d" % n)
-	last_name = 'Steve'
-	email = factory.LazyAttribute(lambda obj: '%s@example.com' % obj.first_name)
+    first_name = factory.Sequence(lambda n: "Agent %03d" % n)
+    last_name = 'Steve'
+    email = factory.LazyAttribute(lambda obj: '%s@example.com' % obj.first_name)
 
-	class UserTest(TestCase):
-		user	=	factories.UserFactory.create()
-        self.client.login(username=user.username, password='abc')
-# Create your tests here.
+
+class UserTest(TestCase):
+    """
+    Test Case for User factory
+    """
+    user = UserFactory()
+
+    def test_user_login_client(self):
+        self.client.login(username=self.user.email, password='abc')
