181743dff8a2cb4e5bdd12099942e16e25a5316b
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-01-15 18:52:56 +0500

    management command to update ipstore

---
+++ apps/common/management/commands/updateipstore.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.core.management import BaseCommand
+from django.apps import apps
+from django.utils import timezone as _tz
+from dateutil.relativedelta import *
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        _delta = _tz.now() + relativedelta(months=-1)
+
+        Impression = apps.get_model('impressions', 'impression')
+        Metric = apps.get_model('guages', 'metric')
+        IPStore = apps.get_model('warehouse', 'ipstore')
+
+        queryset = Impression.objects.filter(
+            meta__at_ip__isnull=False, meta__at_ip2geo__isnull=False)
+
+        print queryset.count()
+
+        for q in queryset:
+            ip = q.meta['ip']
+            location = q.meta['ip2geo']['location']
+
+            store, created = IPStore.objects.get_or_create(ip=ip)
+
+            if created:
+                print "Reverse geocoding against ip: %s" %(store.ip)
+                self.update_gecode(store, location)
+            else:
+                if q.updated_on > _delta and store.updated_on < _delta:
+                    if not (store.latitude == location['latitude'] and store.longitude == location['longitude']):
+                        print "Updating reverse geocoding against ip: %s" %(store.ip)
+                        self.update_gecode(store, location)
+
+    def update_gecode(self, ip, location):
+        from googlemaps import Client
+        from django.conf import settings
+        import json
+
+        ip.latitude = location['latitude']
+        ip.longitude = location['longitude']
+
+        gmaps = Client(key=settings.GOOGLE_GEOCODE_KEY)
+        result = gmaps.reverse_geocode((location['latitude'], location['longitude']))
+        ip.geocode = json.dumps(result)
+
+        ip.save()
