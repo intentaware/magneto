26cb3292c9419265b62dad471649a818b900d020
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-01-15 23:04:44 +0500

    unicode error handling

--- apps/common/management/commands/companyimpressions.py
+++ apps/common/management/commands/companyimpressions.py
@@ -15,7 +15,7 @@ class UnicodeWriter(object):
 
     def writerow(self, D):
         try:
-            self.writer.writerow({k:str(v).encode("utf-8", errors="replace") for k,v in D.items()})
+            self.writer.writerow({k:str(v).encode("utf-8", errors="ignore") for k,v in D.items()})
         except UnicodeEncodeError:
             print "Unicode Error on"
             print D
