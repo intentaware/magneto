04bca4cc503799daf72728ce86b3c2d72b738580
Rehman <manichilll@yahoo.com>
2016-01-21 18:50:14 +0500

    Companies Tests

--- apps/companies/tests.py
+++ apps/companies/tests.py
@@ -6,6 +6,15 @@ class CompaniesFactory(factory.Factory):
     class Meta:
         model = Company
 
-        
+    is_active = True
+
+class CompaniesTest(TestCase):
+    company = CompaniesFactory()
+
+    def test_is_active(self):
+        self.assertEqual(self.company.is_active, True, "Company is_active test Failed.")
+        print "Company is_active test Passed"
+
+
 
 
