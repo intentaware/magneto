b1c0c6ed66e52408732424fb9d9735ba85888f6e
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-09-21 16:37:26 +0500

    quick select campaign for quick summary

--- dashboard/src/js/dashboard/controllers/routes/landing/campaigners.js
+++ dashboard/src/js/dashboard/controllers/routes/landing/campaigners.js
@@ -1,30 +1,52 @@
 'use strict';
 
 angular.module('adomattic.dashboard')
-  .controller('CampaignersLandingCtrl', function($rootScope, $scope, $location, Campaign, Reporter) {
+  .controller('CampaignersLandingCtrl', function($scope, $location, Campaign, Reporter) {
     var urlParamsCampaign = {
       app: 'campaigns',
       endPoint: 'campaigns'
+    };
+
+    $scope.activeCampaign = null;
+
+    $scope.getCampaignReport = function(id) {
+      var params = urlParamsCampaign;
+
+      params.id = id;
+      Reporter.useragents(params).$promise.then(function(d) {
+        $scope.useragents = d;
+      });
+
+      Reporter.history(params).$promise.then(function(d) {
+        $scope.history = [{
+          key: 'History',
+          values: d
+        }];
+      });
     };
 
     var init = function() {
       Campaign.query().$promise.then(function(data) {
         $scope.campaigns = data;
         if (data.length) {
-          var params = urlParamsCampaign;
+          $scope.activeCampaign = data[0].id;
+          $scope.getCampaignReport($scope.activeCampaign);
+          // var params = urlParamsCampaign;
+
           // params.id = data[0].id;
-          params.id = 10;
+          // $scope.activeCampaign = data[0].id;
+          // console.log($scope.activeCampaign);
 
-          Reporter.useragents(params).$promise.then(function(d) {
-            $scope.useragents = d;
-          });
+          // Reporter.useragents(params).$promise.then(function(d) {
+          //   $scope.useragents = d;
+          // });
 
-          Reporter.history(params).$promise.then(function(d) {
-            $scope.history = [{
-              key: 'History',
-              values: d
-            }];
-          });
+          // Reporter.history(params).$promise.then(function(d) {
+          //   $scope.history = [{
+          //     key: 'History',
+          //     values: d
+          //   }];
+          // });
         }
       });
     };
@@ -48,8 +70,12 @@ angular.module('adomattic.dashboard')
           'bottom': 50,
           'left': 50
         },
-        x: function(d){ return d.date; },
-        y: function(d){ return d.count; },
+        x: function(d) {
+          return d.date;
+        },
+        y: function(d) {
+          return d.count;
+        },
         'clipEdge': true,
         'duration': 500,
         'stacked': true,
