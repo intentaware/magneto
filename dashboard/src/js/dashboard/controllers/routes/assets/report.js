7f45d2c53661656633a5a307945f887e373c2587
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2016-10-21 18:37:53 +0500

    datatable for guages

--- dashboard/src/js/dashboard/controllers/routes/assets/report.js
+++ dashboard/src/js/dashboard/controllers/routes/assets/report.js
@@ -1,8 +1,66 @@
 'use strict';
 
 angular.module('adomattic.dashboard')
-  .controller('AssetReportCtrl', function($routeParams) {
+  .controller('AssetReportCtrl', function($routeParams, Reporter, Helper) {
     var self = this;
+    var urlParams = {
+      app: 'guages',
+      endPoint: 'assets',
+      id: $routeParams.assetID
+    };
 
-    self.awesome = 'I am here';
+    self.getData = function() {
+      urlParams.period = self.periodOption;
+      self.data = [];
+      Reporter.datatable(urlParams).$promise.then(function(response) {
+        self.options = {
+          rowHeight: 30,
+          headerHeight: 50,
+          footerHeight: 50,
+          scrollbarV: false,
+          selectable: false,
+          columns: response.columns,
+          // columnMode: 'force',
+          paging: {
+            externalPaging: true,
+            size: 12
+          }
+        };
+
+        self.response = response.data;
+        self.options.paging.count = response.data.length;
+
+        self.paging = function(offset, size) {
+          var set = self.response.splice(offset, size);
+
+          _.forEach(set, function(value, key) {
+            var index = key + offset * size;
+
+            self.data[index] = value;
+          });
+        };
+      });
+    };
+
+    self.periodOptions = [];
+    self.periodOption = 1;
+
+    _.forEach(_.range(1, 13), function(val) {
+      self.periodOptions.push({
+        name: val + ' months',
+        val: val
+      });
+    });
+
+    self.csv = function() {
+      var URL = '/api/guages/assets/' + $routeParams.assetID + '/reports/csv/';
+      var queryParams = Helper.jsonToURL({
+        period: self.periodOption
+      }, URL);
+
+      return queryParams;
+    };
+
+    // init view
+    self.getData();
   });
--- dashboard/src/js/dashboard/controllers/routes/landing/guagues.js
+++ dashboard/src/js/dashboard/controllers/routes/landing/guagues.js
@@ -1,12 +1,52 @@
 'use strict';
 
 angular.module('adomattic.dashboard')
-  .controller('GuagesLandingCtrl', function(Asset, $location) {
+  .controller('GuagesLandingCtrl', function(Asset, Reporter, $location) {
     var self = this;
 
-    Asset.query().$promise.then(function(r){
-      self.assets = r;
-    });
+    var urlParamsReports = {
+      app: 'guages',
+      endPoint: 'assets'
+    };
+
+    self.activeAsset = null;
+
+    self.getAssetReport = function(id) {
+      var params = urlParamsReports;
+
+      params.id = id;
+      Reporter.useragents(params).$promise.then(function(d) {
+        self.useragents = d;
+      });
+
+      Reporter.history(params).$promise.then(function(d) {
+        self.history = [{
+          key: 'History',
+          values: d
+        }];
+      });
+    };
+
+    self.init = function() {
+      Asset.query().$promise.then(function(r) {
+        self.assets = r;
+
+        if (self.assets.length) {
+          self.activeAsset = self.assets[0].id;
+          self.getAssetReport(self.activeAsset);
+        }
+      });
+    };
+
+    self.init();
+
+    self.gotoAssetReport = function(id) {
+      $location.path('/assets/' + id + '/report/');
+    };
+
+    // Asset.query().$promise.then(function(r) {
+    //   self.assets = r;
+    // });
 
     self.goToAssetCreate = function() {
       $location.path('/assets/create/');
